cd73cd9ad5202e21e3de7804582bf8dc2217c04f
gwerrry <46902638+gwerrry@users.noreply.github.com>
2022-03-12 14:14:14 -0500

    small boi upload :(

---
+++ installer/macros-installer.bat
@@ -0,0 +1,110 @@
+@if (@a==@b) @end /*
+
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+Set LATEST_REPO_LINK="https://github.com/Breakfast-Galaxy-Studios/Breakfast-Macros/releases/download/test/app.zip"
+
+for /f "delims=" %%I in ('cscript /nologo /e:jscript "%~f0"') do (
+    echo You chose %%I
+    Set macrosPath=%%I
+)
+
+cd /D %macrosPath%
+mkdir %macrosPath%
+cd %macrosPath%
+
+@echo %macrosPath%
+
+@echo Downloading Macros Latest Version
+
+curl -LO %LATEST_REPO_LINK%
+
+powershell Expand-Archive app.zip -DestinationPath "%macrosPath%" -Force
+
+del app.zip
+
+goto :startmenuchoice
+
+:makestartshortcut
+echo Creating Start Menu shortcut...
+set shortcutlocation="%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Breakfast Macros.lnk"
+set targetpath="%macrosPath%\macros.exe"
+set icon="%macrosPath%\resources\logo.ico"
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%shortcutlocation%');$s.IconLocation='%icon%';$s.TargetPath='%targetpath%';$s.Save()"
+
+:shortcutchoice
+set /P c=Would you like a desktop or a shortcut in another folder? [Y/N]?
+if /I "%c%" EQU "Y" goto :makeshortcut
+if /I "%c%" EQU "N" goto :interceptionchoice
+goto :shortcutchoice
+
+:makeshortcut
+echo Creating Shortcut...
+set targetpath="%macrosPath%\macros.exe"
+set icon="%macrosPath%\resources\logo.ico"
+
+for /f "delims=" %%I in ('cscript /nologo /e:jscript "%~f0"') do (
+    echo You chose %%I
+    Set shortcutlocation="%%I\Breakfast Macros.lnk"
+)
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%shortcutlocation%');$s.IconLocation='%icon%';$s.TargetPath='%targetpath%';$s.Save()"
+
+:interceptionchoice
+
+set /P c="Would you like to install interception(required for first install) [Y/N]?"
+if /I "%c%" EQU "Y" goto :installinterception
+if /I "%c%" EQU "N" goto :EOF
+goto :interceptionchoice
+
+:installinterception
+echo Installing Interception...
+cd /D "%~dp0"
+install-interception.exe /install
+PAUSE
+
+
+goto :EOF
+
+:startmenuchoice
+set /P c=Would you like a start menu shortcut? [Y/N]?
+if /I "%c%" EQU "Y" goto :makestartshortcut
+if /I "%c%" EQU "N" goto :shortcutchoice
+goto :startmenuchoice
+
+
+:: JScript portion */
+
+var shl = new ActiveXObject("Shell.Application");
+var folder = shl.BrowseForFolder(0, "Please choose a folder.", 0, 0x00);
+WSH.Echo(folder ? folder.self.path : '');
